bb46adce50408f0dd1e44700b8435200f1df1935
Bram Janssens <s.a.janssens@gmail.com>
2025-04-16 12:43:14 +0200

    Gatherer.

---
+++ src/main/java/com/infosupport/GathererDemo.java
@@ -0,0 +1,23 @@
+package com.infosupport;
+
+import java.util.stream.Gatherer;
+import java.util.stream.Gatherers;
+import java.util.stream.Stream;
+
+public class GathererDemo {
+
+    public static void main(String[] args) {
+        Stream.generate(() -> (int) (Math.random() * 100))
+                .limit(10)
+                .gather(gt(50))
+                .gather(Gatherers.windowSliding(2))
+                .forEach(System.out::println);
+    }
+
+    public static Gatherer<Number, ?, Number> gt(int threshold) {
+        return Gatherer.ofSequential((_, element, downstream) -> {
+            if (element.intValue() > threshold) downstream.push(element);
+            return true;
+        });
+    }
+}
